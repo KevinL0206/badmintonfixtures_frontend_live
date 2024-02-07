e268e294468322e195dbb4caeac4171e3418144e
Your GitHub Username <k-lam02@outlook.com>
2024-02-07 13:52:43 +0000

    Update favicon and page title and club page

--- frontend/src/component/displaydetails.jsx
+++ frontend/src/component/displaydetails.jsx
@@ -108,7 +108,7 @@ export default function Component(props) {
 
             </main>
             <aside className="w-[21rem] border-l bg-gray-100/40 p-6 md:p-10 dark:bg-gray-800/40">
-                <Collapsible>
+                <Collapsible defaultOpen>
                     <CollapsibleTrigger className="flex items-center gap-2">
                     <ChevronRightIcon className="h-4 w-4" />
                     <span className="text-lg font-semibold">Club Members</span>
