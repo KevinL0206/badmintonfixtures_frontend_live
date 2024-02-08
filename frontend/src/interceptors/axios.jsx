bece5439403c89295bc975935db7af2b4734d942
Your GitHub Username <k-lam02@outlook.com>
2024-02-08 03:12:26 +0000

    Refactor axios interceptor for token refresh

--- frontend/src/interceptors/axios.jsx
+++ frontend/src/interceptors/axios.jsx
@@ -1,62 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-let isRefreshing = false;
-let failedQueue = [];
+let refresh = false;
 
-const processQueue = (error, token = null) => {
-failedQueue.forEach(prom => {
-    if (error) {
-    prom.reject(error);
-    } else {
-    prom.resolve(token);
-    }
-});
-
-failedQueue = [];
-};
-
-axios.interceptors.response.use(response => response, error => {
-const originalRequest = error.config;
-if (error.response.status === 401 && !originalRequest._retry) {
-    if (isRefreshing) {
-    return new Promise((resolve, reject) => {
-        failedQueue.push({resolve, reject});
-    }).then(token => {
-        originalRequest.headers['Authorization'] = 'Bearer ' + token;
-        return axios(originalRequest);
-    }).catch(err => {
-        return Promise.reject(err);
-    });
-    }
-
-    originalRequest._retry = true;
-    isRefreshing = true;
-
-    return new Promise((resolve, reject) => {
-    axios.post('https://badmintonfixtures-71b4cbceb35a.herokuapp.com/token/refresh/', {
-        refresh: localStorage.getItem('refresh_token')
-    }, {
-        headers: {
-        'Content-Type': 'application/json'
+axios.interceptors.response.use(
+resp => resp,
+async error => {
+    if (error.response.status === 401 && !refresh) {
+    refresh = true;
+    const refreshToken = localStorage.getItem('refresh_token');
+    try {
+        const response = await axios.post(
+        'https://badmintonfixtures-71b4cbceb35a.herokuapp.com/refresh/',
+        { refresh: refreshToken },
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
         }
-    }, {withCredentials: true})
-    .then(({data}) => {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.access;
-        localStorage.setItem('access_token', data.access);
-        localStorage.setItem('refresh_token', data.refresh);
-        originalRequest.headers['Authorization'] = 'Bearer ' + data.access;
-        processQueue(null, data.access);
-        resolve(axios(originalRequest));
-    })
-    .catch((err) => {
-        processQueue(err, null);
-        reject(err);
-    })
-    .then(() => {
-        isRefreshing = false;
-    });
-    });
-}
+        );
 
-return Promise.reject(error);
-});
+        if (response.status === 200) {
+        const { access, refresh } = response.data;
+        axios.defaults.headers.common['Authorization'] = `Bearer ${access}`;
+        localStorage.setItem('access_token', access);
+        localStorage.setItem('refresh_token', refresh);
+        return axios(error.config);
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        refresh = false;
+    }
+    }
+    return Promise.reject(error);
+}
+);
